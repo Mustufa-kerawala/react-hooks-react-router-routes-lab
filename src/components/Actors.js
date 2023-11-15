c65dda0b612bba0283a97bcaa9cbf24533866582
Mustufa-kerawala <mustufakerawala@outlook.com>
2023-11-15 13:40:49 +0300

    Completed lab

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,21 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  const actorsTitle = <h1> Actors Page </h1> 
+  const actorsData = actors.map((actor, index) => {
+    return <div key={index}>
+      <h3>{actor.name}</h3>
+      <ul>
+        {actor.movies.map((movie, movieIndex) => <li key={movieIndex}>{movie}</li>)}
+      </ul>
+    </div>
+  })
+
+
+  return <div>
+    {actorsTitle}
+    {actorsData}
+  </div>;
 }
 
 export default Actors;
